9d7a9f91140c7ead3c522cbff711af511321050e
Matthew Chick <c.chick@unsw.edu.au>
2019-11-11 09:41:36 +1100

    npm init, npm install --save-dev jest, write a test

---
+++ script.test.js
@@ -0,0 +1,18 @@
+const googleSearch = require('./script')
+
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com'
+];
+
+it('this is a test', ()=> {
+    //googleSearch('testtest', dbMock);
+    expect('hello').toBe('hello')
+})
+
+it('is searching google', ()=> {  
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+})
